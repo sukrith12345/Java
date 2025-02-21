a8e3a4c2792a13ac3f7a8cade7c8d58bcaf660e4
SUKRITH .K <ksukrith50@gmaIL.COM>
2025-02-21 08:02:18 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,14 @@
+class myMarket {
+  public static void main(String args[]) {
+    int[] myMarket = new int[5]; // Array start with 0 
+    myMarket[0] = 120;
+    myMarket[1] = 50;
+    myMarket[2] = 800;
+    myMarket[3] = 100;
+    myMarket[4] = 69;
+
+    for (int i = 0; i < myMarket.length; i++) {
+      System.out.println("myMarket  in list " + i + " is: " + myMarket[i]);
+    }
+  }
+}
